5044fc34f03e8ea1da259afe46a2f20ca6fd2299
Ulan Djamanbalaev <ulandja@amazon.com>
2026-08-24 17:38:53 +0100

    Make eBay Storefront category IDs optional

--- lib/ebay/types/storefront.rb
+++ lib/ebay/types/storefront.rb
@@ -2,8 +2,8 @@
 module Ebay # :nodoc:
   module Types # :nodoc:
     # == Attributes
-    #  numeric_node :store_category_id, 'StoreCategoryID'
-    #  numeric_node :store_category2_id, 'StoreCategory2ID'
+    #  numeric_node :store_category_id, 'StoreCategoryID', :optional => true
+    #  numeric_node :store_category2_id, 'StoreCategory2ID', :optional => true
     #  text_node :store_category_name, 'StoreCategoryName', :optional => true
     #  text_node :store_category2_name, 'StoreCategory2Name', :optional => true
     #  text_node :store_url, 'StoreURL', :optional => true
@@ -12,8 +12,8 @@ module Ebay # :nodoc:
       include XML::Mapping
       include Initializer
       root_element_name 'Storefront'
-      numeric_node :store_category_id, 'StoreCategoryID'
-      numeric_node :store_category2_id, 'StoreCategory2ID'
+      numeric_node :store_category_id, 'StoreCategoryID', :optional => true
+      numeric_node :store_category2_id, 'StoreCategory2ID', :optional => true
       text_node :store_category_name, 'StoreCategoryName', :optional => true
       text_node :store_category2_name, 'StoreCategory2Name', :optional => true
       text_node :store_url, 'StoreURL', :optional => true
@@ -21,5 +21,3 @@ module Ebay # :nodoc:
     end
   end
 end
-
-
